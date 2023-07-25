6461bc11c7fcbc10b86bd91c8d96023c1a3974a3
D:\temp <sangkoo@naver.com>
2023-07-25 12:11:58 +0900

    add2

--- src/main/java/com/boot/demo/controller/BoardController.java
+++ src/main/java/com/boot/demo/controller/BoardController.java
@@ -2,12 +2,13 @@ package com.boot.demo.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @RequestMapping("/board")
 public class BoardController {
-    @RequestMapping("/list")
+    @GetMapping("/list")
     public String list(Model model){
         return "board/list";
     }
