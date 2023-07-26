10461f5658bc059c7e46187031358162dcb7ed51
D:\temp <sangkoo@naver.com>
2023-07-26 15:43:22 +0900

    add & update

---
+++ src/main/java/com/boot/demo/validator/BoardValidator.java
@@ -0,0 +1,26 @@
+package com.boot.demo.validator;
+
+import com.boot.demo.model.Board;
+import org.springframework.stereotype.Component;
+import org.springframework.validation.Errors;
+import org.springframework.validation.Validator;
+import org.thymeleaf.util.StringUtils;
+
+//아래 사이트 참고
+//https://cjw-awdsd.tistory.com/50
+@Component
+public class BoardValidator implements Validator {
+    @Override
+    public boolean supports(Class<?> clazz) {
+//        return clazz.isAssignableFrom(Board.class);
+        return Board.class.isAssignableFrom(clazz);
+    }
+
+    @Override
+    public void validate(Object target, Errors errors) {
+        Board board = (Board) target;
+        if (StringUtils.isEmpty(board.getContent())){
+            errors.rejectValue("content", "내용을 입력하세요.");
+        }
+    }
+}
